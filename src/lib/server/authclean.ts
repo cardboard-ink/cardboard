036222ce343969e20b0fc652ac2f751112e88ff2
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-05-29 17:41:49 +0530

    fix: cleanup

--- src/lib/server/authclean.ts
+++ src/lib/server/authclean.ts
@@ -4,7 +4,7 @@ export const cleanupAuths = async () => {
     await db.authorizedAppSession.deleteMany({
         where: {
             expiresAt: {
-                lte: new Date()
+                lt: new Date(Date.now())
             }
         }
     })
