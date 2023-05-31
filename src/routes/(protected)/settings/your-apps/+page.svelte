3213f1bc3caedd357e4951dd88356258fb079589
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-05-31 07:44:01 +0530

    fix: onclick propagate

--- src/routes/(protected)/settings/your-apps/+page.svelte
+++ src/routes/(protected)/settings/your-apps/+page.svelte
@@ -44,7 +44,7 @@
             <h5 class="h5">
               {app.id}
             </h5>
-            <button class="btn btn-icon variant-ghost-primary rounded-md" use:clipboard={app.id}>
+            <button class="btn btn-icon variant-ghost-primary rounded-md" use:clipboard={app.id} on:click={(e) => e.stopPropagation()}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24px" class="fill-token"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"/></svg>
             </button>
           </div>
