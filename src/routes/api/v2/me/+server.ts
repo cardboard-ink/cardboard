e8c7766a8894b395e2e4d3a5f5ab8eca40a047a7
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-06-29 21:47:13 +0530

    feat: oauth 2 routes

---
+++ src/routes/api/v2/me/+server.ts
@@ -0,0 +1,53 @@
+import { db } from "$lib/server/database"
+import { error } from "@sveltejs/kit"
+
+export const GET = async ({request}) => {
+    const headers = request.headers
+    let authToken = headers.get('authorization')
+    if (!authToken || typeof authToken !== 'string') {
+        throw error(400, 'invalid header')
+    }
+    if (!authToken.startsWith('Bearer ')) {
+        throw error(400, 'invalid header format')
+    }
+    authToken = authToken.slice(7)
+    const verify = await db.authorizedAppSession.findUnique({
+        where: {
+            authToken,
+        },
+        select: {
+            id: true,
+            userAppManager: {
+                select: {
+                    user: {
+                        select: {
+                            id: true,
+                        }
+                    }   
+                }   
+            }
+        }
+    })
+    if (!verify) {
+        throw error(401, 'invalid token')
+    }
+    const userId = verify.userAppManager.user.id
+    const data = await fetch(`https://www.guilded.gg/api/users/${userId}/profilev3`, {method: 'GET'}).catch((e) => {
+        throw error(500, `internal server error: ${e}`)
+    })
+    const userData = await data.json()
+    const resData = {
+        id: userData.id,
+        name: userData.name,
+        subdomain: userData.subdomain,
+        aliases: userData.aliases,
+        avatar: userData.profilePictureLg,
+        banner: userData.profileBannerLg,
+        createdAt: userData.joinedAt,
+        userStatus: userData.userStatus,
+        moderationStatus: userData.moderationStatus,
+        aboutInfo: userData.aboutInfo,
+        userTransientStatus: userData.userTransientStatus,
+    }
+    return new Response(JSON.stringify(resData), {headers: {'Content-Type': 'application/json'}})
+}
---
+++ src/routes/api/v2/token/+server.ts
@@ -0,0 +1,121 @@
+import { db } from "$lib/server/database"
+import { error } from "@sveltejs/kit"
+import { randomUUID } from "crypto"
+
+export const POST = async ({request}) => {
+    console.log(request)
+    const data = await request.formData().catch(() => {
+        throw error(400, `invalid format, needs to be multipart/form-data`)
+    })
+    const grantType = data.get('grant_type')
+    const authToken = data.get('code')
+    const redirectUri = data.get('redirect_uri')
+    const refreshToken = data.get('refresh_token')
+
+    if ( 
+        !authToken ||
+        !redirectUri || 
+        !grantType || 
+        typeof grantType !== 'string' || 
+        typeof authToken !== 'string' || 
+        typeof redirectUri !== 'string'
+        ) 
+    {
+        throw error(400, 'invalid request')
+    }
+
+    if (grantType === 'authorization_code') {
+        if (!authToken || typeof authToken !== 'string') {
+            throw error(400, 'invalid request')
+        }
+        const pre = await db.authorizedAppSession.findUnique({
+            where: {
+                authToken,
+            },
+            select: {
+                id: true,
+                userAppManager: {
+                    select: {
+                        app: {
+                            select: {
+                                id: true,
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        if (!pre) {
+            throw error(400, 'invalid code, has it expired?')
+        }
+        const auth = await db.authorizedAppSession.update({
+            where: {
+                id: pre.id,
+            },
+            data: {
+                authToken: randomUUID(),
+                refreshToken: randomUUID(),
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+            },
+            select: {
+                authToken: true,
+                refreshToken: true,
+                expiresAt: true,
+            }
+        })
+        const data = {
+            access_token: auth.authToken,
+            expires_in: Math.floor((auth.expiresAt.getTime() - Date.now()) / 1000),
+            refresh_token: auth.refreshToken,
+            token_type: 'Bearer',
+        }
+        return new Response(JSON.stringify(data), {headers: {'Content-Type': 'application/json'}})
+    } else if (grantType === 'refresh_token') {
+        if (!refreshToken || typeof refreshToken !== 'string') {
+            throw error(400, 'invalid request')
+        }
+        const pre = await db.authorizedAppSession.findUnique({
+            where: {
+                refreshToken,
+            },
+            select: {
+                id: true,
+                userAppManager: {
+                    select: {
+                        app: {
+                            select: {
+                                id: true,
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        if (!pre) {
+            throw error(400, 'invalid code, has it expired?')
+        }
+        const auth = await db.authorizedAppSession.update({
+            where: {
+                id: pre.id,
+            },
+            data: {
+                refreshToken: randomUUID(),
+                authToken: randomUUID(),
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+            },
+            select: {
+                authToken: true,
+                refreshToken: true,
+                expiresAt: true,
+            }
+        })
+        const data = {
+            access_token: auth.authToken,
+            expires_in: Math.floor((auth.expiresAt.getTime() - Date.now()) / 1000),
+            refresh_token: auth.refreshToken,
+            token_type: 'Bearer',
+        }
+        return new Response(JSON.stringify(data), {headers: {'Content-Type': 'application/json'}})
+    }
+    throw error(400, 'unsupported grant_type')
+}
