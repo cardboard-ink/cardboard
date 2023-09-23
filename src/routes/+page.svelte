1505dd1206fccc9c6b43f26eff8655ca01871cdd
NotAussie <NotAussie@duck.com>
2023-09-23 13:44:06 +0930

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,17 +23,17 @@
   {:else}
     <h2 class="p">{greeting()}, visitor! 👋</h2>
     <p>Welcome to CardBoard, the all in one Guilded authentication serivce. Get started by signing in!</p>
-    <a href="/login"><button class="rounded bg-primary-500 p-4">Sign in 🔑</button></a>
+    <a href="/login"><button class="rounded bg-primary-500 p-3">Sign in 🔑</button></a>
   {/if}
   <div class="flex flex-col gap-4 mb-4 md-4 relative">
     {#if $page.data.user}
       <br>
       <a href="https://www.guilded.gg/CardBoard/groups/3y446Rmz/channels/4539a4f9-fb51-4a23-b014-0fcaeaf062d3/docs/374610" target="_blank">
-        <button class="rounded bg-primary-500 p-4">Learn the ropes of CardBoard. ✨</button>
+        <button class="rounded bg-primary-500 p-3">Learn the ropes of CardBoard. ✨</button>
       </a>
       <br>
       <a href="https://www.guilded.gg/CardBoard/groups/3y446Rmz/channels/4539a4f9-fb51-4a23-b014-0fcaeaf062d3/docs/374610" target="_blank">
-        <button class="rounded bg-primary-500 p-4">Get started with developing on CardBoard. 💻</button>
+        <button class="rounded bg-primary-500 p-3">Get started with developing on CardBoard. 💻</button>
       </a>
     {/if}
   </div>
