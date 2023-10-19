c3c9356919942a2e75a59403cc30e848c57f985f
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-10-20 02:20:34 +0530

    fix: removed openapi creator

--- src/routes/api/openapi.json/+server.ts
+++
@@ -1,7 +0,0 @@
-import api from "$api";
-import { json } from "@sveltejs/kit";
-import type { RequestHandler } from "./$types";
-
-export const prerender = true;
-
-export const GET = (async (evt) => json(await api.openapi(evt))) satisfies RequestHandler;
