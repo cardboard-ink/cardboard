61ebef56054eca7c37377015fb3be594f244e1ac
Soham Jaiswal <sohamjaiswal10@gmail.com>
2024-01-29 06:07:58 +0530

    feat: url check

--- src/lib/utils/guilded-media.ts
+++ src/lib/utils/guilded-media.ts
@@ -1,5 +1,6 @@
 export const guildedMediaLink = (awsUrl: string) => {
   // replace https://s3-us-west-2.amazonaws.com/www.guilded.gg with https://cdn.gilcdn.com 
   // keep everything else same
+  if (!awsUrl) return awsUrl;
   return awsUrl.replace('https://s3-us-west-2.amazonaws.com/www.guilded.gg', 'https://cdn.gilcdn.com');
 }
