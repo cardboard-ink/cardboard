1e393c6b568cd0279e4bb25ab79bce03ab4a94e3
Soham Jaiswal <sohamjaiswal10@gmail.com>
2024-07-06 17:15:50 +0530

    fix: app specific token check

--- src/routes/api/v1/token/check/+server.ts
+++ src/routes/api/v1/token/check/+server.ts
@@ -1,32 +1,68 @@
-import { db } from "$lib/server/database"
-import { error } from "@sveltejs/kit"
+import { db } from '$lib/server/database';
+import { error } from '@sveltejs/kit';
+import bcrypt from 'bcrypt';
 
-export const POST = async({request}) => {
+export const POST = async ({ request }) => {
+	const data = await request.formData().catch(() => {
+		throw error(400, `invalid format, needs to be multipart/form-data`);
+	});
 
-    const data = await request.formData().catch(() => {
-        throw error(400, `invalid format, needs to be multipart/form-data`)
-    })
+	const token = data.get('token');
+	const clientId = data.get('client_id');
+	const clientSecret = data.get('client_secret');
 
-    const token = data.get('token')
+	if (
+		!token ||
+		!clientId ||
+		!clientSecret ||
+		typeof clientId !== 'string' ||
+		typeof clientSecret !== 'string' ||
+		typeof token !== 'string'
+	) {
+		throw error(400, 'invalid request');
+	}
 
-    if (!token || typeof token !== 'string') {
-        throw error(400, 'invalid request')
-    }
+	let validity = false;
 
-    let validity = false
+	const pre = await db.authorizedAppSession.findUnique({
+		where: {
+			authToken: token
+		},
+		select: {
+			id: true,
+			userAppManagerId: true
+		}
+	});
 
-    const pre = await db.authorizedAppSession.findUnique({
-        where: {
-            authToken: token,
-        },
-        select: {
-            id: true,
-        }
-    })
+	if (pre) {
+		const manager = await db.userAppManager.findUnique({
+			where: {
+				id: pre.userAppManagerId
+			},
+			select: {
+				appId: true
+			}
+		});
 
-    if (pre) {
-        validity = true
-    }
+		const app = await db.app.findUnique({
+			where: {
+				id: clientId
+			},
+			select: {
+				id: true,
+				secret: true
+			}
+		});
 
-    return new Response(JSON.stringify({validity}), {headers: {'Content-Type': 'application/json'}})
-}
+		if (manager && app && app.id === manager.appId) {
+			const comparision = await bcrypt.compare(clientSecret, app.secret);
+			if (comparision) {
+				validity = true;
+			}
+		}
+	}
+
+	return new Response(JSON.stringify({ validity }), {
+		headers: { 'Content-Type': 'application/json' }
+	});
+};
